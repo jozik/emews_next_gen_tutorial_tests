cabe414351e8334af54ad001d507e6433501a882
Nick Collier <ncollier@anl.gov>
2022-01-12 17:21:20 -0500

    Image dir fix

--- scripts/build_site.sh
+++ scripts/build_site.sh
@@ -4,9 +4,6 @@ set -eu
 THIS=$( cd $( dirname $0 ) ; /bin/pwd )
 SITE=$THIS/../docs
 WEBSITE=https://repast.github.io/emews_next_gen_tutorial_tests
-IMAGES=$THIS/../docs/images
-
-
 
 echo "Building site"
-asciidoctor -a website=$WEBSITE -a imagesdir=$IMAGES $THIS/../src/main.adoc -o $SITE/index.html
+asciidoctor -a website=$WEBSITE $THIS/../src/main.adoc -o $SITE/index.html
