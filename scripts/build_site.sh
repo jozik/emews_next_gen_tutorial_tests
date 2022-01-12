291587bb1d6f1c69a9cdd9d2581e066495a23382
Nick Collier <ncollier@anl.gov>
2022-01-12 17:30:52 -0500

    Updated code link

--- scripts/build_site.sh
+++ scripts/build_site.sh
@@ -3,9 +3,6 @@ set -eu
 
 THIS=$( cd $( dirname $0 ) ; /bin/pwd )
 SITE=$THIS/../docs
-WEBSITE=https://repast.github.io/emews_next_gen_tutorial_tests
 
 echo "Building site"
-asciidoctor -a website=$WEBSITE $THIS/../src/main.adoc -o $SITE/index.html
-
-https://jozik.github.io/emews_next_gen_tutorial_tests/images/shared_net_2.png
+asciidoctor $THIS/../src/main.adoc -o $SITE/index.html
