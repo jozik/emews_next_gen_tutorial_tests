a4664b653c96af90f6760594ae6d9fdd805ca2b9
Jonathan Ozik <jozik@uchicago.edu>
2024-01-12 10:24:45 -0600

    Removed comment from build script.

--- scripts/build_site.sh
+++ scripts/build_site.sh
@@ -7,4 +7,4 @@ SITE=$THIS/../docs
 echo "Building site"
 # Install asciidoctor-bibtex. E.g., gem install asciidoctor-bibtex
 asciidoctor -r asciidoctor-bibtex $THIS/../src/main.adoc -o $SITE/index.html
-#asciidoctor -r asciidoctor-bibtex src/main.adoc -o docs/index.html
+
