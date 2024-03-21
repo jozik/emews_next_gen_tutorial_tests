27bfd42cd549829e0bffea800f5300a824be4771
Justin Wozniak <woz@anl.gov>
2024-03-21 13:31:49 -0500

    Bring in R example

---
+++ src/snippets/stats.swift
@@ -0,0 +1,21 @@
+import io;
+import string;
+import files;
+import R;
+
+app (file o) simulation(int i) {
+  "bash" "-c"
+    ("RANDOM=%i; echo $RANDOM" % i)
+    @stdout=o;
+}
+
+string results[];
+foreach i in [0:9] {
+  f = simulation(i);
+  results[i] = read(f);
+}
+
+A = join(results, ",");
+code = "m = mean(c(%s))" % A;
+mean = R(code, "toString(m)");
+printf(mean);
