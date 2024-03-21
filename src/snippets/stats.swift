e3104922819df5935e855776bebc4f7ad6e518fa
Justin Wozniak <woz@anl.gov>
2024-03-21 18:49:05 -0500

    Use callouts

--- src/snippets/stats.swift
+++ src/snippets/stats.swift
@@ -1,21 +1,21 @@
-import io;
+import io;                          // <1>
 import string;
 import files;
 import R;
 
-app (file o) simulation(int i) {
+app (file o) simulation(int i) {    // <2>
   "bash" "-c"
     ("RANDOM=%i; echo $RANDOM" % i)
     @stdout=o;
 }
 
-string results[];
+string results[];                   // <3>
 foreach i in [0:9] {
   f = simulation(i);
   results[i] = read(f);
 }
 
-A = join(results, ",");
+A = join(results, ",");             // <4>
 code = "m = mean(c(%s))" % A;
 mean = R(code, "toString(m)");
 printf(mean);
