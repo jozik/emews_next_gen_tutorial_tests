c6d99a7e0eec8c91d6980ca307670b542f0bd838
Justin Wozniak <woz@anl.gov>
2024-04-11 15:14:09 -0500

    New installer test

---
+++ jenkins/install.sh
@@ -0,0 +1,42 @@
+#!/bin/zsh
+set -eu
+
+# JENKINS INSTALL SH
+# Runs install_emews.sh
+
+DATE_FMT_NICE="%D{%Y-%m-%d} %D{%H:%M:%S}"
+log()
+# General-purpose log line
+{
+  print ${(%)DATE_FMT_NICE} "setup-conda.sh:" ${*}
+}
+
+log "JENKINS INSTALL SH"
+
+THIS=${0:h:A}
+EMEWS=${THIS:h}
+
+# Defaults:
+PYTHON_VERSION=${PYTHON_VERSION:-311}
+CONDA_LABEL=${CONDA_LABEL:-23.11.0-1}
+DB=$WORKSPACE/DB
+
+# Main argument processing
+zparseopts -D -E -F c:=CL p:=PV u=UNINSTALL
+if (( ${#PV} )) PYTHON_VERSION=${PV[2]}
+if (( ${#CL} )) CONDA_LABEL=${CL[2]}
+
+MINICONDA=$WORKSPACE/Miniconda-${PYTHON_VERSION}_${CONDA_LABEL}
+log "MINICONDA: $MINICONDA"
+if [[ ! -d $MINICONDA ]] {
+  log "Not found: MINICONDA=$MINICONDA"
+  exit 1
+}
+
+# Convert, e.g., "311" -> "3.11"
+PV_DOT=3.${PYTHON_VERSION[2,-1]}
+
+renice --priority 19 --pid ${$} >& /dev/null
+
+set -x
+$EMEWS/code/install/install_emews.sh $PV_DOT $DB
