893ee42618b413f9d9ce15c9fdc15012ae2beff7
Justin Wozniak <woz@anl.gov>
2024-12-03 14:14:24 -0600

    Drop gitcreds - it is not needed

--- code/install/install_eq_sql.R
+++ code/install/install_eq_sql.R
@@ -7,24 +7,5 @@
 #                    use it via ~/.Renviron.
 #      https://snyk.io/blog/how-to-use-github-actions-environment-variables
 
-## if (Sys.getenv("GITHUB_PAT") != "") {
-##   print("GITHUB_PAT detected.  Installing gitcreds...")
-
-##   r <- getOption("repos")
-##   # Change this mirror as needed:
-##   # r["CRAN"] <- "http://cran.cnr.berkeley.edu/"
-##   # r["CRAN"] <- "http://cran.wustl.edu/"
-##   # r["CRAN"] <- "https://mirror.las.iastate.edu/"
-##   r["CRAN"] <- "https://cloud.r-project.org"
-##   options(repos = r)
-
-##   # Install gitcreds!
-##   install.packages("gitcreds", verbose=TRUE)
-
-##   print("")
-##   print("LOAD:    gitcreds")
-##   library(gitcreds)
-## }
-
 # Install EQ.SQL!
 remotes::install_github('emews/EQ-SQL/R/EQ.SQL')
